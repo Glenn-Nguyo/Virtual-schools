34b08ac19f1f8e94613306d7c8d6babbf622a3e3
Nicholas <nicholas.nmalinda@gmail.com>
2022-11-01 15:18:19 +0300

    created QuestionList component

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,45 +1,45 @@
-// import React, {useState, useEffect} from 'react';
-// import Question from './Question';
+import React, {useState, useEffect} from 'react';
+import Question from './Question';
 
 
-// export default function QuestionList(){
-//     const [questionList, setQuestionList] = useState([]);
+export default function QuestionList(){
+    const [questionList, setQuestionList] = useState([]);
 
-//     function handleQuestionFetch(){
-//         fetch ('https://virtualschools.herokuapp.com/questions')
-//         .then ((res)=>res.json())
-//         .then ((questions)=>{
-//             setQuestionList(questions);
-//         })
+    function handleQuestionFetch(){
+        fetch ('https://virtualschools.herokuapp.com/questions')
+        .then ((res)=>res.json())
+        .then ((questions)=>{
+            setQuestionList(questions);
+        })
 
-//     }
-//     useEffect(handleQuestionFetch, [])
+    }
+    useEffect(handleQuestionFetch, [])
 
 
-//     function handleQuestionUpdate(updatedQuestion){
-//         const updatedQuestionList = questionList.map((question) => {
-//           if(question.id === updatedQuestion.id){
-//             return updatedQuestion;
-//           }else{
-//             return question
-//           }
-//         });
-//         setQuestionList(updatedQuestionList);
-//     }
+    function handleQuestionUpdate(updatedQuestion){
+        const updatedQuestionList = questionList.map((question) => {
+          if(question.id === updatedQuestion.id){
+            return updatedQuestion;
+          }else{
+            return question
+          }
+        });
+        setQuestionList(updatedQuestionList);
+    }
 
 
     
 
 
-//      let questions = questionList.map((item)=>{
-//          return (
-//         <Question handleQuestionUpdate={handleQuestionUpdate} key={item.id}questionList={questionList}  setQuestionList={setQuestionList} question={item.question} questionId={item.id}/>
-//       ) })
+     let questions = questionList.map((item)=>{
+         return (
+        <Question handleQuestionUpdate={handleQuestionUpdate} key={item.id}questionList={questionList}  setQuestionList={setQuestionList} question={item.question} questionId={item.id}/>
+      ) })
 
 
-//      return (
-//         <div>
-//              {questions}                    
-//          </div>
-//     );    
-// }
+     return (
+        <div>
+             {questions}                    
+         </div>
+    );    
+}
