bc81f8c6fa73f1109c2c0553b18a50fef654662d
Nicholas <nicholas.nmalinda@gmail.com>
2022-11-01 15:20:46 +0300

    created UdateQuestion component

--- src/components/UpdateQuestion.js
+++ src/components/UpdateQuestion.js
@@ -1,69 +1,69 @@
-// import React, {useState} from 'react'; 
-// import {useParams} from 'react-router-dom'
-// import CreateQuestions from './CreateQuestions';
+import React, {useState} from 'react'; 
+import {useParams} from 'react-router-dom'
+import CreateQuestions from './CreateQuestions';
 
 
-// function UpdateQuestion({handleQuestionUpdate}){
+function UpdateQuestion({handleQuestionUpdate}){
 
-//     const [updatedQuestion, setUpdatedQuestion] = useState({
-//         question: "",
-//     })
+    const [updatedQuestion, setUpdatedQuestion] = useState({
+        question: "",
+    })
 
-//      let {questionId} = useParams()
+     let {questionId} = useParams()
 
-//     function handleChange(e){
-//         setUpdatedQuestion({...updatedQuestion, [e.target.name]: e.target.value})
+    function handleChange(e){
+        setUpdatedQuestion({...updatedQuestion, [e.target.name]: e.target.value})
 
-//     }
+    }
 
-//     console.log(updatedQuestion)
+    console.log(updatedQuestion)
 
-//     function handleSubmit(e){
-//         e.preventDefault();
+    function handleSubmit(e){
+        e.preventDefault();
          
 
-//         const updatedObj = {
-//             question: updatedQuestion.question,
-//             points: updatedQuestion.points,
-//             exam_id: updatedQuestion.exam_id
-//         }
+        const updatedObj = {
+            question: updatedQuestion.question,
+            points: updatedQuestion.points,
+            exam_id: updatedQuestion.exam_id
+        }
 
-//             fetch(`https://virtualschools.herokuapp.com/questions/${questionId}`,{
+            fetch(`https://virtualschools.herokuapp.com/questions/${questionId}`,{
         
-//              method: "PATCH",
-//              headers: {
-//                  "Content-Type": "application/json",
-//              },
-//              body: JSON.stringify(updatedObj),
+             method: "PATCH",
+             headers: {
+                 "Content-Type": "application/json",
+             },
+             body: JSON.stringify(updatedObj),
 
-//          })
-//          .then((res) => res.json())
-//          .then((updatedQuestion) => handleQuestionUpdate(updatedQuestion))
-//     }
+         })
+         .then((res) => res.json())
+         .then((updatedQuestion) => handleQuestionUpdate(updatedQuestion))
+    }
 
-//     return (
-//         <div className="updateForm">
-//             <form onSubmit={handleSubmit}>
-//                     <h3>Udate Question</h3>
-//                 <div>
-//                     <label for="question" className="form-label">Question</label>
-//                     <input
-//                         className="form-control"
-//                         type="text"
-//                         name="question"
-//                         value= {updatedQuestion.question}
-//                         onChange={handleChange}
-//                         placeholder="question"/>
-//                 </div>
+    return (
+        <div className="updateForm">
+            <form onSubmit={handleSubmit}>
+                    <h3>Udate Question</h3>
+                <div>
+                    <label for="question" className="form-label">Question</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="question"
+                        value= {updatedQuestion.question}
+                        onChange={handleChange}
+                        placeholder="question"/>
+                </div>
                
-//                <div>
-//                <input
-//                     className=" mt-4 btn btn-primary"type="submit"/>
-//                </div>
-//             </form>
-//         </div>
-//     )
-// }
+               <div>
+               <input
+                    className=" mt-4 btn btn-primary"type="submit"/>
+               </div>
+            </form>
+        </div>
+    )
+}
 
-// export default UpdateQuestion;
+export default UpdateQuestion;
 
