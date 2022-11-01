0fc63bf6d55b94da3a1a5d45c56a07d9535ea0cc
Nicholas <nicholas.nmalinda@gmail.com>
2022-11-01 15:17:04 +0300

    ft created Question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,52 +1,52 @@
-// import React from 'react';
-// import {Link} from 'react-router-dom'
-// import UpdateQuestion from './UpdateQuestion';
-// import { confirmAlert } from 'react-confirm-alert'
+import React from 'react';
+import {Link} from 'react-router-dom'
+import UpdateQuestion from './UpdateQuestion';
+import { confirmAlert } from 'react-confirm-alert'
 
-// function Question({key, question, questionId, setQuestionList, questionList, handleQuestionUpdate}){
+function Question({key, question, questionId, setQuestionList, questionList, handleQuestionUpdate}){
 
-//   console.log(questionId)
+  console.log(questionId)
 
-//   function handleDelete(){
+  function handleDelete(){
 
-//     confirmAlert({
-// 			title: "Confirm deletion",
-// 			message:"All data will be lost. Wish to continue?",
-// 			buttons: [
-// 				{
-// 					label: "Yes",
-// 					onClick : ()=> handleDelete()
-// 				},
-// 				{
-// 					label: "No",
-// 					onClick: () => null
-// 				}
-// 			]
-// 		})
-//     fetch(`https://virtualschools.herokuapp.com/questions/${questionId}`,{
-//       method: "DELETE"
-//     })
-//     .then((res) => res.json())
-//     .then(()=>{
-//      setQuestionList(questionList.filter(question => question.id !== questionId))
+    confirmAlert({
+			title: "Confirm deletion",
+			message:"All data will be lost. Wish to continue?",
+			buttons: [
+				{
+					label: "Yes",
+					onClick : ()=> handleDelete()
+				},
+				{
+					label: "No",
+					onClick: () => null
+				}
+			]
+		})
+    fetch(`https://virtualschools.herokuapp.com/questions/${questionId}`,{
+      method: "DELETE"
+    })
+    .then((res) => res.json())
+    .then(()=>{
+     setQuestionList(questionList.filter(question => question.id !== questionId))
     
-//     })
+    })
 
     
-//   }
+  }
 
-//   <UpdateQuestion handleQuestionUpdate={handleQuestionUpdate}/>
+  <UpdateQuestion handleQuestionUpdate={handleQuestionUpdate}/>
 
-//     return (
-//     <div key={key}>
-//       <h4><em>Q {questionId}</em>: {question}</h4>
-//       <Link to={"/question/" + questionId} className="btn btn-secondary">Update</Link>
-//       <button className="btn btn-warning mt-1" onClick={handleDelete}>Delete</button>
-//     </div>
+    return (
+    <div key={key}>
+      <h4><em>Q {questionId}</em>: {question}</h4>
+      <Link to={"/question/" + questionId} className="btn btn-secondary">Update</Link>
+      <button className="btn btn-warning mt-1" onClick={handleDelete}>Delete</button>
+    </div>
 
-//     )
-// }
+    )
+}
 
-// export default Question;
+export default Question;
 
 
