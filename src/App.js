3dea115d555960f2987d5248036f706b7813a3c4
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-11-03 12:39:39 +0300

    update routes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Educators from "./components/Educators/Educators";
 function App() {
   return (
     <BrowserRouter>
-    <Header />
+/    <Header />
       <Routes>
       <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
